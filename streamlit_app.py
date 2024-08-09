15f3413c43b3dc3d698863f3d3025dbcdbc74b1e
ashishgupta-97065 <ashishgupta97065@gmail.com>
2024-08-09 21:27:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,31 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+questions = [1,2,3]
+
+num1 = [1,2,3]
+num2 = [4,5,6]
+
+for question in questions:
+    with st.form("my_form"):
+        st.write("Question number: ", question)
+        
+        n1 = num1[question]
+        n2 = num2[question]
+
+        sum = n1 + n2
+        
+        st.write(n1," + ", n2)
+        ans=st.number_input(" Enter your answer",step=1)
+        
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.write("Question: ", n1, " + ", n2)
+            st.write("Actual Answer: ", sum)
+            st.write("Your Answer: ", ans)
+            
+            if ans==sum:
+                st.write( " YAY YOU ARE CORRRRRRRREEEEEECCCCCCTTTTTTTTTTTTTTTTTT")
+            else:
+                st.write(" YOU GET A RED CAAAAAAAAARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRD")
+
+    st.write("Outside the form")
