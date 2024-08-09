9f2dc85506598d80e5d99fa406a9a41206725d72
ashishgupta-97065 <ashishgupta97065@gmail.com>
2024-08-09 21:30:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,27 +10,27 @@ num1 = [1,2,3]
 num2 = [4,5,6]
 
 for question in questions:
+  form_num = "Question" + question
+  with st.form(form_num):
+    st.write("Question number: ", question)
     
-    with st.form("Question",question):
-        st.write("Question number: ", question)
+    n1 = num1[question]
+    n2 = num2[question]
+  
+    sum = n1 + n2
+    
+    st.write(n1," + ", n2)
+    ans=st.number_input(" Enter your answer",step=1)
+    
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("Question: ", n1, " + ", n2)
+        st.write("Actual Answer: ", sum)
+        st.write("Your Answer: ", ans)
         
-        n1 = num1[question]
-        n2 = num2[question]
-
-        sum = n1 + n2
-        
-        st.write(n1," + ", n2)
-        ans=st.number_input(" Enter your answer",step=1)
-        
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            st.write("Question: ", n1, " + ", n2)
-            st.write("Actual Answer: ", sum)
-            st.write("Your Answer: ", ans)
-            
-            if ans==sum:
-                st.write( " YAY YOU ARE CORRRRRRRREEEEEECCCCCCTTTTTTTTTTTTTTTTTT")
-            else:
-                st.write(" YOU GET A RED CAAAAAAAAARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRD")
+        if ans==sum:
+            st.write( " YAY YOU ARE CORRRRRRRREEEEEECCCCCCTTTTTTTTTTTTTTTTTT")
+        else:
+            st.write(" YOU GET A RED CAAAAAAAAARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRD")
 
     st.write("Outside the form")
